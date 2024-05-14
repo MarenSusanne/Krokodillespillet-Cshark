756b67752fd6b3c6c50ef2439b80481483e6c284
MarenSusanne <Marensut@getacademy.no>
2024-05-14 14:27:07 +0200

    Add project files.

---
+++ Krokodillespillet C#/Program.cs
@@ -0,0 +1,67 @@
+﻿    int points = 0;
+rules();main();
+void main()
+{
+    
+    int rnd1 = rand();
+    int rnd2 = rand();
+    Console.WriteLine($"Dine Poeng:{points}");
+    Console.WriteLine($"{rnd1} _ {rnd2}");
+    string answer = Console.ReadLine();
+    if (rnd1 == rnd2)
+    {
+        if (answer == "=")
+        {
+            points++;
+            Console.WriteLine("Correct!");
+        }
+        else
+        {
+            points--;
+            Console.WriteLine("Wrong!");
+        }
+    }
+    else if (rnd1 < rnd2)
+    {
+        if (answer == "<")
+        {
+            points++;
+            Console.WriteLine("Correct!");
+        }
+        else
+        {
+            points--;
+            Console.WriteLine("Wrong!");
+        }
+    }
+    else if (rnd1 > rnd2)
+    {
+        if (answer == ">")
+        {
+            points++;
+            Console.WriteLine("Correct!");
+        }
+        else
+        {
+            points--;
+            Console.WriteLine("Wrong!");
+        }
+    }
+    main();
+}
+
+void rules()
+{
+    Console.WriteLine("Hvilket av tallene er størst?");
+    Console.WriteLine("Regler:");
+    Console.WriteLine("Bruk < , > eller = for å si hvilket tall som er størst");
+    Console.WriteLine("Ved riktig svar, får du 1 poeng.");
+    Console.WriteLine("Ved feil svar, mister du 1 poeng.");
+}
+
+int rand()
+{
+    var rnd = new Random();
+    var rndNumber = rnd.Next(0, 20);
+    return rndNumber;
+}
